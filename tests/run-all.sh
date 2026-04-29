6d949b8c1dea18970be0a53d9f34f230afde85b9
F.R.I.D.A.Y. <265173460+mk-friday@users.noreply.github.com>
2026-04-28 23:06:27 -0400

    Enable debug logging in staged app env

--- tests/run-all.sh
+++ tests/run-all.sh
@@ -12,6 +12,7 @@ tests=(
   "$TEST_DIR/test-runtime-wrapper.sh"
   "$TEST_DIR/test-selinux-policy-installer.sh"
   "$TEST_DIR/test-selinux-policy-installer-mock.sh"
+  "$TEST_DIR/test-stage-env-patch.sh"
 )
 
 for test_script in "${tests[@]}"; do
